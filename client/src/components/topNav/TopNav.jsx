43675bbe73f4139c7304c6d796192a162607968a
Leon <hq_li1982@hotmail.com>
2021-07-24 22:54:51 +0800

    half way of topnav component

--- client/src/components/topNav/TopNav.jsx
+++ client/src/components/topNav/TopNav.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+
+import Dropdown from '../dropdown/Dropdown';
+
 import './topNav.css';
 
 const TopNav = () => {
   return (
-    <div>
-      
+    <div className="topnav">
+      <div className="topnav-search">
+        <input type="text" placeholder="Search here..."/>
+        <i className='bx bx-search'></i>
+      </div>
+      <div className="topnav-right">
+        <div className="topnav-right-item">
+          {/* usr profile dropdown */}
+          <Dropdown />
+        </div>
+        <div className="topnav-right-item">
+          {/* notification dropdown */}
+          <Dropdown />
+        </div>
+        <div className="topnav-right-item">
+          {/* Theme setting dropdown */}
+          <Dropdown />
+        </div>
+      </div>
     </div>
   )
 }
