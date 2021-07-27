cb59412d989ab56b33dae69299bf6fc7bb821055
Leon <hq_li1982@hotmail.com>
2021-07-27 22:44:55 +0800

    customer page

--- client/src/components/dropdown/Dropdown.jsx
+++ client/src/components/dropdown/Dropdown.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import './dropdown.css';
 
-const Dropdown = () => {
+const clickOutsideRef = (contentRef, toggleRef) => {
+  document.addEventListener('mousedown', (e) => {
+    // user click toggle
+    // console.log(toggleRef)
+    if (toggleRef.current && toggleRef.current.contains(e.target)) {
+      contentRef.current.classList.toggle('active')
+    } else {
+      // user click outside toggle and content
+      if (contentRef.current && !contentRef.current.contains(e.target)) {
+        contentRef.current.classList.remove('active')
+      }
+    }
+  })
+}
+
+const Dropdown = props => {
+
+  const dropdownToggle = useRef(null);
+  const dropdownContent = useRef(null);
+
+  clickOutsideRef(dropdownContent, dropdownToggle);
+
   return (
-    <div>
-      Dropdown
+    <div className="dropdown">
+      <button ref={dropdownToggle} className="dropdown-toggle">
+        {
+          props.icon ? <i className={props.icon}></i> : ''
+        }
+        {
+          props.badge ? <span className="dropdown-toggle-badge">{props.badge}</span> : ''
+        }
+        {
+          props.customToggle ? props.customToggle() : ''
+        }
+      </button>
+      <div ref={dropdownContent} className="dropdown-content">
+        {
+          props.contentData && props.renderItems ? props.contentData.map((item, index) => props.renderItems(item, index)) : ''
+        }
+        {
+          props.renderFooter ? (
+            <div className="dropdown-footer">
+              {props.renderFooter()}
+            </div>
+          ) : ''
+        }
+      </div>
     </div>
   )
 }
